cbbb61a368ed1e42aa7b1d6597d7d79bcbc82750
karteek.puthani <karteek.puthani@mouser.com>
2026-05-27 12:20:05 +0530

    commit 1

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  cloud {
+    organization = "terraform-karthik"
+    workspaces {
+      name = "tf-cloud-test"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.01"
+    }
+  }
+}
+
+
+# Variable blocks directly within the main.tf. No arguments necessary.
+# variable "aws_access_key" {}
+# variable "aws_secret_key" {}
+variable "region" {}
+
+# provider arguments call on the variables which then call on terraform.tfvars for the values.
+provider "aws" {
+#   access_key = var.aws_access_key
+#   secret_key = var.aws_secret_key
+  region     = var.region
+}
+
+# Add .gitignore file in this directory with the terraform.tfvars
+
+resource "aws_instance" "tc_instance" {
+  ami           = "ami-0c7c4e3c6b4941f0f"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "TC-triggered-instance"
+  }
+}
